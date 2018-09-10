bad1cc0e8a9ed6d338b24be893327f13f5d76f6d
WANDEE SREPENRAK <59050300@kmitl.ac.th>
2018-09-10 18:47:17 +0700

    add : adder.java

---
+++ Adder.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Adder {
+    public static void main(String[] args) {
+        // Input
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println(x * y);
+        // Process
+        //int z = x + y;
+
+        // Output
+        //System.out.println(z);
+    }
+}
